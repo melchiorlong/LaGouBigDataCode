30b8a8134c836557a6a501e6b3c6cd16431b69f1
melchiorlong <448704865@qq.com>
2020-08-05 01:24:49 +0800

    commit

--- com/study/Stage1/Section2/Person/Person.java
+++ com/study/Stage1/Section2/Person/Person.java
@@ -2,7 +2,15 @@ package com.study.Stage1.Section2.Person;
 
 public class Person {
 
+    String name;
+    int age;
+
     public static void main(String[] args) {
+        Person p = new Person();
+        System.out.println("我的名字是" + p.name + ",我的年龄是" + p.age);
+        p.name = "long0312";
+        p.age = 26;
+        System.out.println("我的名字是" + p.name + ",我的年龄是" + p.age);
 
     }
 }
