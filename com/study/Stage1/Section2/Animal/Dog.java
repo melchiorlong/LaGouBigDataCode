edb499943812d86ca93ad779b4598ed3aa6102a2
melchiorlong <448704865@qq.com>
2020-08-13 05:06:21 +0800

    person

---
+++ com/study/Stage1/Section2/Animal/Dog.java
@@ -0,0 +1,32 @@
+package com.study.Stage1.Section2.Animal;
+
+public class Dog extends Animal {
+
+    private int teeth;
+
+    public int getTeeth() {
+        return teeth;
+    }
+
+    public void setTeeth(int teeth) {
+        if (teeth > 0){
+            this.teeth = teeth;
+        }else{
+            System.out.println("牙齿数量错误");
+        }
+    }
+
+    public Dog() {
+    }
+
+    public Dog(String name, String color, int teeth) {
+        super(name, color);
+        setTeeth(teeth);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        System.out.println("牙齿数量" + teeth);
+    }
+}
---
+++ com/study/Stage1/Section2/Animal/DogTest.java
@@ -0,0 +1,17 @@
+package com.study.Stage1.Section2.Animal;
+
+public class DogTest {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.show();
+
+        System.out.println("----------------------------------------");
+
+        Dog dog1 = new Dog("皮皮","白色",20);
+        dog1.show();
+
+    }
+
+}
