d9a0d2baf7a7ed822bfbefc598a4255a3fa43289
melchiorlong <448704865@qq.com>
2020-08-12 21:42:49 +0800

    commit

--- com/study/Stage1/Section2/People/People.java
+++ com/study/Stage1/Section2/People/People.java
@@ -3,8 +3,47 @@ package com.study.Stage1.Section2.People;
 public class People {
     private String name;
     private int age;
-    private String nationality;
+    public static String nationality;
 
+    public People(){}
+    public People(String name, int age, String nationality){
+        setName(name);
+        setAge(age);
+        setNationality(nationality);
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age > 0 && age <150){
+            this.age = age;
+        }else{
+            System.out.println("年龄有误");
+        }
+    }
+
+    public void setNationality(String nationality) {
+        People.nationality = nationality;
+    }
+
+    public void show(){
+        System.out.println("姓名：" + getName());
+        System.out.println("年龄：" + getAge());
+        System.out.println("国籍：" + getNationality());
+    }
 
 }
