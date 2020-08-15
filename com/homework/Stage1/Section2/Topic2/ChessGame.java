6e0bb91ad0bd3e29a00a3098ffc8921407890933
melchiorlong <448704865@qq.com>
2020-08-16 00:17:37 +0800

    person

--- com/homework/Stage1/Section2/Topic2/ChessGame.java
+++ com/homework/Stage1/Section2/Topic2/ChessGame.java
@@ -30,6 +30,7 @@ public class ChessGame {
         List<String> edge_list = Arrays.asList(edge);
         do {
             Scanner sc = new Scanner(System.in);
+//            输出下棋顺序，白先黑后
             System.out.println(whichOne > 0 ? "白方下棋，请出入横纵坐标（如：1 2）" : "黑方下棋，请出入横纵坐标（如：1 2）");
             String inx = sc.next();
             String iny = sc.next();
@@ -38,22 +39,23 @@ public class ChessGame {
                 System.out.println("位置越界，请重新输入坐标");
                 continue;
             }
+//            输入字符串转为16进制Long类型后强转为int
             int x = (int) Long.parseLong(inx, 16);
             int y = (int) Long.parseLong(iny, 16);
 //            判断输入坐标是否已经被使用
             if (!chess_board[x][y].equals("+")) {
-                System.out.println("位置已经被使用，请重新输入坐标");
+                System.out.println("该位置已经被使用，请重新输入坐标");
                 continue;
             }
-//          更新数组
+//            更新数组
             update(x, y, whichOne, chess_board);
-//          交换对手
+//            交换对手
             whichOne *= -1;
 //            打印棋盘
             insert();
             print(chess_board);
 
-//          判断是否结束及谁是胜者
+//            判断是否结束及谁是胜者
             for (int i = 0; i < length; i++) {
                 for (int j = 0; j < length; j++) {
                     if (!isFinished) {
@@ -93,15 +95,17 @@ public class ChessGame {
 
     /**
      * 判断胜负
+     *
      * @return 返回值 1：黑胜 -1：白胜
      */
+
     public int whoIsWinner(String[][] chess_board, int x, int y) {
         int piecesCount = 0;
         int result = 1;
         boolean flag = false;
         String currentPieces = chess_board[x][y];
-        // 水平垂直判断
-        // 左 - 右
+//        水平垂直判断
+//        左 - 右
         if (!currentPieces.equals("+")) {
             for (int i = y; i < length; i++) {
                 if (chess_board[x][i].equals(currentPieces)) {
@@ -115,7 +119,7 @@ public class ChessGame {
             } else {
                 piecesCount = 0;
             }
-            // 右 - 左
+//            右 - 左
             for (int i = y; i >= 0; i--) {
                 if (chess_board[x][i].equals(currentPieces)) {
                     piecesCount++;
@@ -128,7 +132,7 @@ public class ChessGame {
             } else {
                 piecesCount = 0;
             }
-            // 上 - 下
+//            上 - 下
             for (int i = x; i < length; i++) {
                 if (chess_board[i][y].equals(currentPieces)) {
                     piecesCount++;
@@ -141,7 +145,7 @@ public class ChessGame {
             } else {
                 piecesCount = 0;
             }
-            // 下 - 上
+//            下 - 上
             for (int i = x; i >= 0; i--) {
                 if (chess_board[i][y].equals(currentPieces)) {
                     piecesCount++;
@@ -156,8 +160,8 @@ public class ChessGame {
             }
 
 
-            // 斜向
-            // 左上 - 右下
+//            斜向
+//            左上 - 右下
             for (int i = x, j = y; i < length && j < length; i++, j++) {
                 if (chess_board[i][j].equals(currentPieces)) {
                     piecesCount++;
@@ -171,7 +175,7 @@ public class ChessGame {
             } else {
                 piecesCount = 0;
             }
-            // 右下 - 左上
+//            右下 - 左上
             for (int i = x, j = y; i >= 0 && j >= 0; i--, j--) {
                 if (chess_board[i][j].equals(currentPieces)) {
                     piecesCount++;
@@ -184,7 +188,7 @@ public class ChessGame {
             } else {
                 piecesCount = 0;
             }
-            // 左下 - 右上
+//            左下 - 右上
             for (int i = x, j = y; i < length && j >= 0; i++, j--) {
                 if (chess_board[i][j].equals(currentPieces)) {
                     piecesCount++;
@@ -197,7 +201,7 @@ public class ChessGame {
             } else {
                 piecesCount = 0;
             }
-            // 右下 - 左上
+//            右下 - 左上
             for (int i = x, j = y; i >= 0 && j < length; i--, j++) {
                 if (chess_board[i][j].equals(currentPieces)) {
                     piecesCount++;
@@ -226,35 +230,35 @@ public class ChessGame {
     }
 
     private void print(String[][] chess_board) {
-        // 打印图中左上角空格
+//        打印图中左上角空格
         System.out.print(" ");
-        // 循环打印坐标数组作为行坐标
+//        循环打印坐标数组作为行坐标
         for (int i = 0; i < size; i++) {
             System.out.print("  " + str[i]);
         }
-        // 换行
+//        换行
         System.out.println();
 
-        // 双重for循环打印其余部分
+//        双重for循环打印其余部分
         for (int i = 0; i < size; i++) {
             // 打印坐标数组作为列坐标
             System.out.print(str[i] + "  ");
             for (int j = 0; j < size; j++) {
-                // 打印棋盘中心“+”
+//                打印棋盘中心“+”
                 System.out.print(chess_board[i][j] + "  ");
             }
-            // 换行
+//            换行
             System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        // 绘制棋盘
+//        绘制棋盘
         ChessGame cg = new ChessGame();
         ChessBoardCons cbc = new ChessBoardCons();
         cbc.generating();
         String[][] chess_board = cbc.getChess_board();
-        // 提示黑方和白方分别下棋、重新绘制棋盘
+//        提示黑方和白方分别下棋、重新绘制棋盘
         cg.start(chess_board);
     }
 }
